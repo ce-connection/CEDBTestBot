07759b1cd3959ee2b2c55691057aa199bad85749
David <david.azoherra@gmail.com>
2019-06-24 23:18:12 -0600

    commit inicial

---
+++ src/main/java/com/ce/testBot.java
@@ -0,0 +1,38 @@
+package com.ce;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+/**
+ * Hello world!
+ *
+ */
+public class testBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "tearsintherainbot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "812895708:AAFC6ooyk0tYWGhuN2f1leKhCWIbWZubob0";
+    }
+
+}
